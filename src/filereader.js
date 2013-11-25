b20f8e91b2e0b4c844a6f20cc99c01cce92556c7
Anton <Feverqwe@users.noreply.github.com>
2013-11-25 16:46:56 +0600

    x2 speed reading ID3v2 tags.
    Need add ID3v2.readSynchsafeInteger32At = readSynchsafeInteger32At;
    in ID3v2

--- src/filereader.js
+++ src/filereader.js
@@ -13,6 +13,21 @@
                 var result = event.target.result;
                 fncCallback(new BinaryFile(result));
             };
+            var isID3v2 = true;
+            reader.onprogress = function(event) {
+                if (isID3v2 === false || event.target.result.length < 28) {
+                    return;
+                }
+                isID3v2 = (event.target.result.substr(0, 3) === "ID3");
+                if (isID3v2) {
+                    var data = new BinaryFile(event.target.result);
+                    var size = ns.ID3v2.readSynchsafeInteger32At(6, data);
+                    if (size > 0) {
+                        fncCallback(data);
+                        event.target.abort();
+                    }
+                }
+            };
             reader.readAsBinaryString(file);
         }
     };
